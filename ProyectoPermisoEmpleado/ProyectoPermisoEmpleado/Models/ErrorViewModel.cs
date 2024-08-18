ae0f2ba051dab04b90d91e22e505888be17a892f
KCarrera1998 <117527849+KCarrera1998@users.noreply.github.com>
2024-08-18 15:50:35 -0600

    InicioProyecto
    1

---
+++ ProyectoPermisoEmpleado/ProyectoPermisoEmpleado/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoPermisoEmpleado.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
